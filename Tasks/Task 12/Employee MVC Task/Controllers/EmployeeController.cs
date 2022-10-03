f5dd5d1474dbb3647589597d6d96463bfbc54c1a
dhanushs1409 <113021717+dhanushs1409@users.noreply.github.com>
2022-10-03 16:23:38 +0530

    Add files via upload

---
+++ Tasks/Task 12/Employee MVC Task/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeTask.Models;
+
+namespace EmployeeTask.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult EmployeeInfo()
+        {
+            Employee emp = new Employee();
+            emp = emp.GetEmployee();
+            return View(emp);
+        }
+        public IActionResult AllEmployeeInfo()
+        {
+            List<Employee> empList = Employee.GetAllEmployee();
+            return View(empList);
+        }
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tasks/Task 12/Employee MVC Task/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿namespace EmployeeTask.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public char Gender { get; set; }
+        public Employee GetEmployee() //return a single employee
+        {
+            Employee emp1 = new Employee()
+            {
+                EmpID = 101,
+                FName = "Ajay",
+                LName = "Devgan",
+                Gender = 'M'
+            };
+            return emp1;
+        }
+        public static List<Employee> GetAllEmployee() //returns list
+        {
+            List<Employee> empList = new List<Employee>()
+            {
+                new Employee(){ EmpID=101, FName="Ajay", LName="Devgan", Gender='M'},
+                new Employee(){ EmpID=102, FName="Shruti", LName="Iyer", Gender='F'},
+                new Employee(){ EmpID=103, FName="Akshara", LName="Modi", Gender='F'},
+                new Employee(){ EmpID=104, FName="Anirudh", LName="Ashwin", Gender='M'},
+                new Employee(){ EmpID=105, FName="Narendra", LName="Khan", Gender='M'},
+                new Employee(){ EmpID=106, FName="Priya", LName="Sharma", Gender='F'}
+            };
+            return empList;
+        }
+    }
+}
